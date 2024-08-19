201507883f1245e4ae977b8485f36647d15a623d
Guto Hertzog <guto.hertzog@gmail.com>
2024-08-19 14:25:53 -0300

    adicionado conexao com o postgresql e tratamento de erros

--- README.md
+++ README.md
@@ -4,13 +4,11 @@ Esse positório sugiu surgiu a partir de uma demanda do trabalho.
 
 Necessidade : realizar a atualização dos dados de uma tabela no banco de dados PostgreSQL a paritr de um arquivo no formato xlsx.
 
-Numa primeira versão, o script Python apenas irá buscar os dados no arquivo xlsx e então gerar as queries,
-que serão executas no pgAdmin.
-
 Para realizar isso, é necessário usar algumas bibliotecas externas do Python.
 
 Bibliotecas :
 - openpyxl : usado para abrir e carregar os dados do arquivo xlsx;
+- psycopg2 : usado para realizar a conexão com o banco de dados PostgreSQL;
 
 PS.: por motivos de segurança, nomes de tabelas e campos serão generalizados.
 
--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 '''
-v.1.1
+v.1.2
 
 é esperado que o arquivo venha com 9 colunas [0..8],
 sendo apenas as duas abaixo importantes, por hora.
     - [4] cpf
-    - [6] situação
+    - [6] vínculo
 '''
 
 import time
 import openpyxl
+import psycopg2
+
+from getpass import getpass
 
 inicio = time.time()  # cronômetro tempo de execução
 
-ARQ_SQL = 'atualiza.sql'
 ARQ_XLSX = 'arquivo-carga.xlsx'
 
+print('lendo o arquivo .xlsx')
 planilha = openpyxl.load_workbook(ARQ_XLSX)
 aba = planilha.active
 
@@ -22,33 +25,62 @@ matrix = []
 for linha in aba.iter_rows(values_only=True):
     matrix.append(linha)
 
-# a partir da matrix, cria todas as queries
-queries = []
-for linha in matrix:
-    # descara os cabeçalhos
-    if len(linha < 8):
-        continue
-    if len(linha[4]) <= 3:
-        continue
+# contador para os registros atualizados
+contador = 0
+try:
+    print('\nestabelecendo conexão com o banco de dados\n')
+    usuario = input('\tdigite o usuário : ')
+    senha = getpass('\tdigite a senha : ')
+    ip_server = input('\tdigite o IP : ')
+    banco = input('\tdigite o banco : ')
+    porta = 5432
 
-    if linha[6] == 'ATIVO':
-        sit = 2
-    elif linha[6] == 'INATIVO':
-        sit = 4
-    elif linha[6] == 'SEM INFORMACAO':
-        sit = 5
-    else:
-        print('\n\n\t\terro de vínculo desconhecido\n')
-        print(f'{linha = }\n')
-        exit()
+    conexao = psycopg2.connect(
+        database = banco,
+        user     = usuario,
+        password = senha,
+        host     = ip_server,
+        port     = porta)
 
-    cpf = str(linha[4]).zfill(11)
-    query = f'update <tabela> set vinculo = {sit} '
-    query += f"where cpf = '{cpf}' and curso_id in (5,6);"
+    print('\nconxão bem sucedida')
+    cursor = conexao.cursor()
 
-with open(ARQ_SQL, 'w', encoding='utf-8') as arq:
-    for query in queries:
-        arq.write(query + '\n')
+    print('\natualizando registros')
+    for linha in matrix:
+        # descarta os cabeçalhos
+        if isinstance(linha[4], str):
+            continue
+
+        if linha[6] == 'ATIVO':
+            vinc = 2
+        elif linha[6] == 'INATIVO':
+            vinc = 4
+        elif linha[6] == 'SEM INFORMACAO':
+            vinc = 5
+        else:
+            raise TypeError(linha)
+
+        cpf = str(linha[4]).zfill(11)
+        query = 'update <tabela> set vinculo = %s'
+        query += 'where cpf = %s and curso_id in (5,6)'
+
+        cursor.execute(query, (vinc, cpf))
+        contador += cursor.rowcount
+
+    coexao.commit()
+    print('operação finalizada')
+
+    cursor.close()
+    conexao.close()
+    print('conexão com o banco fechada')
+
+except TypeError as erro:
+    print('\n\t\terro de vínculo desconhecido\n')
+    print(f'{erro = }\n')
+
+except (Exception, psycopg2.Error) as erro:
+    print('\n\terro ao tentar atualizar no banco\n')
+    print(f'{erro = }\n')
 
 fim = time.time()
 print(f'terminado em {fim - inicio:.2f} segundos')
